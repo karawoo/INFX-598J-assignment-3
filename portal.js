9effb954cf1c4e9e9c42a83a3cc6c2e5630a2abb
Kara Woo <woo.kara@gmail.com>
2016-02-15 21:41:14 -0800

    Implement pan and zoom! Closes #8
    Squashed commit of the following:
    
    commit 79c873ebcea58687bb180fc17c8b9d7b424da50f
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:39:19 2016 -0800
    
        Another comment
    
    commit 07a13696046ac7f04f8c2caee40d311cdd310b4c
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:36:53 2016 -0800
    
        Constrain zooming
    
    commit 0ec4fd7fb8e49b2be00e3cd07dac28d75ffbaf18
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:36:09 2016 -0800
    
        Add scrolling/zooming instructions to page
    
    commit c3071ba8a8d8c3c2ff733935f959f3f91a0ed980
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:33:02 2016 -0800
    
        Comments
    
    commit a8ff47f598dc896cf9b7df51e49431ab46e4993d
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:31:40 2016 -0800
    
        Set minima according to the data
    
    commit 1a36dabf6d2c43395f16559cf2688c9d37bfa568
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:29:19 2016 -0800
    
        Rename xCat and yCat to xVar and yVar
    
    commit 5d082d1bb5c63c03559d91406daa1ed38e63cd25
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 21:28:15 2016 -0800
    
        Remove commented out x/y domains
    
    commit 3ad895c7d02c38a11a42740fe5f59ae2d7b8756e
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 20:38:19 2016 -0800
    
        Points need to be placed with transform for panning/zooming to work properly
    
    commit d5f92fa428324d2a31715c3e56e343688a6b37c0
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 20:37:38 2016 -0800
    
        ymin and xmin can just be zero
    
    commit 1feb68992604b025402fde5216b5eeea80620f0c
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 20:36:27 2016 -0800
    
        Move colors and tooltips out of global namespace
    
    commit f0970fce0de94e378180ed9d0010038ed583416e
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 19:37:56 2016 -0800
    
        Don't multiply xMax by 1.05
    
    commit 608bc58bbb423b62202c75232d780069cc9c0b49
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 19:36:12 2016 -0800
    
        Shuffle code around
    
        Works marginally better now?
    
    commit 4f31e9b36e56fe19df2306c5389f051d3f543b91
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 19:36:00 2016 -0800
    
        Remove x axis checkbox
    
    commit d69283b380acaeda3912f832625d90d181a938a0
    Author: Kara Woo <woo.kara@gmail.com>
    Date:   Mon Feb 15 15:30:33 2016 -0800
    
        Zooming almost sort of works!

--- portal.js
+++ portal.js
@@ -4,57 +4,21 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50};
 var w = 900 - margin.left - margin.right;
 var h = 500 - margin.top - margin.bottom;
 
-// Create SVG
-var svg = d3.select("body").append("svg")
-        .attr("width", w + margin.left + margin.right)
-        .attr("height", h + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 // x and y scales 
 var x = d3.scale.linear()
-        .domain([0, 300])
-        .range([0, w]);
+        .range([0, w])
+        .nice();
 
 var y = d3.scale.linear()
-        .domain([0, 70])
-        .range([h, 0]);
+        .range([h, 0])
+        .nice();
 
-// Draw x axis
-var xAxis = d3.svg.axis()
-        .ticks(6)
-        .scale(x);
-
-// Draw y axis
-var yAxis = d3.svg.axis()
-        .ticks(7)
-        .scale(y)
-        .orient("left");
-
-// Colors for genera
-// Source: http://jnnnnn.blogspot.com.au/2015/10/selecting-different-colours-for.html
-var col = d3.scale.ordinal()
-        .domain(["Dipodomys", "Chaetodipus", "Onychomys", "Reithrodontomys",
-                 "Peromyscus", "Perognathus", "Neotoma", "Ammospermophilus",
-                 "Amphispiza", "Spermophilus", "Sigmodon", "Sylvilagus",
-                 "Pipilo", "Campylorhynchus", "Baiomys", "Callipepla",
-                 "Calamospiza", "Rodent", "Pooecetes", "Sceloporus", "Lizard",
-                 "Sparrow", "Ammodramus", "Cnemidophorus", "Crotalus",
-                 "Zonotrichia"])
-        .range(["#a0cb76", "#6aa3d5", "#dcaf98", "#b6692e", "#a76a59",
-                "#04908e", "#d771ab", "#a69683", "#8268d0", "#72ab79", "#f70c8b",
-                "#ebaa4c", "#9ce7b8", "#5f837a", "#df708c", "#ad9c32", "#39ffc2",
-                "#d28388", "#79d5f9", "#e35eff", "#ffaf72", "#55e0b3", "#e8c0fe",
-                "#6a69ed", "#fe07d3", "#0c86af"]);
-
-// Tooltips for points
-var tooltip = d3.select("body").append("div") .attr("class", "tooltip")
-        .style("opacity", 0);
+var xVar = "weight",
+    yVar = "hindfoot_length";
 
 // Load data
-var portal;
 d3.csv("portal_combined.csv", function(error, data) {
-    //read in the data
+    // Read in the data
     if (error) return console.warn(error);
     data.forEach(function(d) { // Following columns should be numeric
         d.month = +d.month;
@@ -90,38 +54,102 @@ d3.csv("portal_combined.csv", function(error, data) {
     //             };
     //         })
     //         .entries(data);
+
+    // Calculate maxima and minima and use these to set x/y domain
+    var xMax = d3.max(data, function(d) { return d[xVar]; }),
+        xMin = d3.min(data, function(d) { return d[xVar]; }),
+        yMax = d3.max(data, function(d) { return d[yVar]; }),
+        yMin = d3.min(data, function(d) { return d[yVar]; });
+
+    x.domain([xMin, xMax]);
+    y.domain([yMin, yMax]);
+
+    // Draw x axis
+    var xAxis = d3.svg.axis()
+            .ticks(6)
+            .scale(x);
+
+    // Draw y axis
+    var yAxis = d3.svg.axis()
+            .ticks(7)
+            .scale(y)
+            .orient("left");
+
+    // Colors for genera
+    // Source: http://jnnnnn.blogspot.com.au/2015/10/selecting-different-colours-for.html
+    var col = d3.scale.ordinal()
+            .domain(["Dipodomys", "Chaetodipus", "Onychomys", "Reithrodontomys",
+                     "Peromyscus", "Perognathus", "Neotoma", "Ammospermophilus",
+                     "Amphispiza", "Spermophilus", "Sigmodon", "Sylvilagus",
+                     "Pipilo", "Campylorhynchus", "Baiomys", "Callipepla",
+                     "Calamospiza", "Rodent", "Pooecetes", "Sceloporus", "Lizard",
+                     "Sparrow", "Ammodramus", "Cnemidophorus", "Crotalus",
+                     "Zonotrichia"])
+            .range(["#a0cb76", "#6aa3d5", "#dcaf98", "#b6692e", "#a76a59",
+                    "#04908e", "#d771ab", "#a69683", "#8268d0", "#72ab79",
+                    "#f70c8b", "#ebaa4c", "#9ce7b8", "#5f837a", "#df708c",
+                    "#ad9c32", "#39ffc2", "#d28388", "#79d5f9", "#e35eff",
+                    "#ffaf72", "#55e0b3", "#e8c0fe", "#6a69ed", "#fe07d3",
+                    "#0c86af"]);
+
+    // Tooltips for points
+    var tooltip = d3.select("body").append("div") .attr("class", "tooltip")
+            .style("opacity", 0);
     
-    portal = data;
-    drawVis(portal);
+    // Zooming
+    var zooming = d3.behavior.zoom()
+            .x(x)
+            .y(y)
+            .scaleExtent([0.5, 10])
+            .on("zoom", zoomFun);
+    
+    // Create SVG
+    var svg = d3.select("#scatter")
+            .append("svg")
+            .attr("width", w + margin.left + margin.right)
+            .attr("height", h + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(zooming);
+    
+    // Add x axis label ("Weight")
+    svg.append("g")
+        .classed("x axis", true)
+        .attr("transform", "translate(0," + h + ")")
+        .call(xAxis)
+        .append("text")
+        .classed("label", true)
+        .attr("x", w)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Weight");
 
-});
+    // Add y axis label ("Hindfoot length")
+    svg.append("g")
+        .classed("y axis", true)
+        .call(yAxis)
+        .append("text")
+        .classed("label", true)
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Hindfoot Length");
 
+    // Create svg rectangle
+    svg.append("rect")
+        .attr("width", w)
+        .attr("height", h);
 
-// Add x axis label ("Weight")
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + h + ")")
-    .call(xAxis)
-    .append("text")
-    .attr("x", w)
-    .attr("y", -6)
-    .style("text-anchor", "end")
-    .text("Weight");
+    // Create `objects` to hold points
+    var objects = svg.append("svg")
+            .classed("objects", true)
+            .attr("width", w)
+            .attr("height", h);
 
-// Add y axis label ("Hindfoot length")
-svg.append("g")
-    .attr("class", "axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Hindfoot Length");
-
-function drawVis() {
-    svg.selectAll(".dot")
-        .data(portal
+    // Plot points
+    objects.selectAll(".dot")
+        .data(data
               .filter(function(d){
                   if(isNaN(d.hindfoot_length)){
                       return false;
@@ -139,9 +167,8 @@ function drawVis() {
         .enter()
         .append("circle")
         .attr("class", "dot")
-        .attr("cx", function(d) { return x(d.weight);  })
-        .attr("cy", function(d) { return y(d.hindfoot_length);  })
         .attr("r", 5)
+        .attr("transform", transform)
         .style("fill", function(d) { return col(d.genus); })
         .style("stroke", function(d) {return col(d.genus); })
         .on("mouseover", function(d) { tooltip.transition()
@@ -173,7 +200,7 @@ function drawVis() {
 
         // Helper function that will return correct display value for each dot
         function display(d) {
-            // Check if the current dot"s sex and plot type are present in
+            // Check if the current dot's sex and plot type are present in
             // `sexes` and `plottypes`
             if (sexes.indexOf(d.sex) !== -1
                 && plottypes.indexOf(d.plot_type) !== -1) {
@@ -183,13 +210,25 @@ function drawVis() {
             }
         }
 
-        // change display attribute of every dot using display function
+        // Change display attribute of every dot using display function
         svg.selectAll(".dot").attr("display", display);
     }
 
-    // update every time a checkbox changes
+    // Update every time a checkbox changes
     d3.selectAll(".filter_options input").on("change", function() {
         update();
     });
-    
-}
+
+    // Zooming function
+    function zoomFun() {
+        svg.select(".x.axis").call(xAxis);
+        svg.select(".y.axis").call(yAxis);
+
+        svg.selectAll(".dot")
+            .attr("transform", transform);
+    }
+
+    function transform(d) {
+        return "translate(" + x(d[xVar]) + "," + y(d[yVar]) + ")";
+    }
+});
